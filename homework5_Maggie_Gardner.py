746c3342d276db27db8808a95ae8644929b7cb61
magcgard <magcgard@uw.edu>
2026-02-09 04:46:56 +0000

    homework 5 complete

---
+++ homework5_Maggie_Gardner.py
@@ -0,0 +1,14 @@
+books = ["Tomorrow,Tomorrow,Tomorrow", "Demon Copperhead", "North Woods", "The Nix", "East of Eden", "The Island of Doctor Moreau"]
+print(books)
+first2 = books[0: 2: 1]
+print ("the first two items in the list are", first2[0], ", ", first2[1])
+middle2 = books[2: 4: 1]
+print ("the middle two items in the list are", middle2[0], ", ", middle2[1] )
+print ("the first and last two items in the list are", books[0], ", ", books[5] )
+menu = ("sandwich", "curry", "katsu", "cake", "spatzle") 
+for food in menu:
+    print(food)
+menu2= ("boiled eggs", "ramen", "katsu", "cake", "spatzle") 
+for food in menu2:
+    print(food)
+    
